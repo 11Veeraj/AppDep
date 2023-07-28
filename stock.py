d422842ed257dfe208f6311c542d8c965e9600e8
Veeraj Goudar <80123055+11Veeraj@users.noreply.github.com>
2023-07-28 15:49:17 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import missingno as msno
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.svm import SVR
